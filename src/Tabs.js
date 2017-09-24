7402bdfcb748370447e096e52be9dd748335de1f
abhishek <kris.abs@gmail.com>
2017-09-24 23:22:03 +0100

    Modify tab titles. Add underwater.css for displaying images. Add all current underwater madhuban comics

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import ReactTabs, { Tabs, TabList, TabPanel, Tab } from 'react-tabs';
-import './react-tabs.css' 
+import './react-tabs.css'
+import './underwatermadhuban.css'
 
 class TabsWrapper extends Component {
 	render() {
 		return(
 			<Tabs>
 				<TabList>
-					<Tab> Test 1 </Tab>
-					<Tab> Test 2 </Tab>
+					<Tab> About </Tab>
+					<Tab> Underwater Madhuban </Tab>
+					<Tab> Words </Tab>
+					<Tab> Games </Tab>
 				</TabList>
 				<TabPanel>
 					<h2> Test 1 is here </h2>
 					</TabPanel>
 				<TabPanel>
-					<h2> Test 2 is here </h2>
+					<div id="underwaterimages">
+					<img src={ require('./UnderwaterMadhuban/1.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/2.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/3.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/4.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/5.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/6.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/7.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/8.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/9.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/10.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/11.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/12.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/13.jpg') } width="500" height="500"/>
+					<img src={ require('./UnderwaterMadhuban/14.jpg') } width="500" height="500"/>
+					</div>
 				</TabPanel>
 			</Tabs>
 		);
