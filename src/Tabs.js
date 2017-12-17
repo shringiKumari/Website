a070ea46d3ec7389846bc65ab77182d4c5cd0f3b
Shringi <sk1382@york.ac.uk>
2017-12-17 21:07:00 +0000

    style changes

--- src/Tabs.js
+++ src/Tabs.js
@@ -14,21 +14,20 @@ class TabsWrapper extends Component {
 					<Tab> Games </Tab>
 				</TabList>
 				<TabPanel>
-					<div classname="underwaterimages">
+					<div id="underwaterimages">
 					<img src={ require('./UnderwaterMadhuban/1.jpg') } width="700" />
+					</div>
+					<div id="underwaterimages">
 					<img src={ require('./UnderwaterMadhuban/2.jpg') } width="700" />
+					</div>
+					<div id="underwaterimages">
 					<img src={ require('./UnderwaterMadhuban/3.jpg') } width="700" />
+					</div>
+					<div id="underwaterimages">
 					<img src={ require('./UnderwaterMadhuban/4.jpg') } width="700" />
+					</div>
+					<div id="underwaterimages">
 					<img src={ require('./UnderwaterMadhuban/5.jpg') } width="700" />
-					<img src={ require('./UnderwaterMadhuban/6.jpg') } width="700" />
-					<img src={ require('./UnderwaterMadhuban/7.jpg') } width="700" />
-					<img src={ require('./UnderwaterMadhuban/8.jpg') } width="700" />
-					<img src={ require('./UnderwaterMadhuban/9.jpg') } width="700" />
-					<img src={ require('./UnderwaterMadhuban/10.jpg') } width="700" />
-					<img src={ require('./UnderwaterMadhuban/11.jpg') } width="700" />
-					<img src={ require('./UnderwaterMadhuban/12.jpg') } width="700" />
-					<img src={ require('./UnderwaterMadhuban/13.jpg') } width="700" />
-					<img src={ require('./UnderwaterMadhuban/14.jpg') } width="700" />
 					</div>
 				</TabPanel>
 			</Tabs>
